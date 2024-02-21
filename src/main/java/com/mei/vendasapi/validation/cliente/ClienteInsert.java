de7185c5b0caec9edd9573d6e6eac715e71cf33d
João Carlos <102131443+J0AO0@users.noreply.github.com>
2024-02-20 21:21:03 -0300

    Cliente validator

---
+++ src/main/java/com/mei/vendasapi/validation/cliente/ClienteInsert.java
@@ -0,0 +1,15 @@
+package com.mei.vendasapi.validation.cliente;
+
+import com.mei.vendasapi.validation.categoria.CategoriaInsertValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CategoriaInsertValidator.class)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClienteInsert {
+}
---
+++ src/main/java/com/mei/vendasapi/validation/cliente/ClienteUpdate.java
@@ -0,0 +1,4 @@
+package com.mei.vendasapi.validation.cliente;
+
+public @interface ClienteUpdate {
+}
